f984ad520d1f46626d9da64b5cfb459f9005bdc7
Surfingnet <maximeghazarian1@gmail.com>
2016-02-19 14:21:11 +0100

    Question 1

--- src/index.php
+++ src/index.php
@@ -1,7 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Surf
- * Date: 19/02/2016
- * Time: 13:12
- */
+
+include "./includes/conn.php";
+
+function question1(string $nom, string $identite_secrete, int $id_super_pouvoir)//:bool
+{
+    // >>> Require $conn comme connexion PDO avec la DB.
+    try
+    {
+        $sql = "INSERT
+        INTO `super_hero`
+        (`nom`,
+         `identite_secrete`,
+         `id_super_pouvoir`)
+        VALUES
+        ( :nom,
+         :identite_secrete,
+         :id_super_pouvoir);";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':nom', $nom);
+        $stmt->bindParam(':identite_secrete', $identite_secrete);
+        $stmt->bindParam(':id_super_pouvoir', $id_super_pouvoir);
+        $stmt->execute();
+
+        return true;
+    }
+    catch (PDOException $e)
+    {
+        echo $e->getMessage();
+        return false;
+    }
+
+}
