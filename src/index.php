446fa17e28d01bd40b8d7c800af49284d66e7bb7
Surfingnet <maximeghazarian1@gmail.com>
2016-02-19 14:50:30 +0100

    Question 3

--- src/index.php
+++ src/index.php
@@ -56,3 +56,23 @@ function question2(string $nom)//:[]
         return [];
     }
 }
+
+function question3(string $nom, string $identite_secrete, int $id_super_pouvoir)//:bool
+{
+    try
+    {
+        $conn = new PDO('mysql:dbname=PartielPHP;host=127.0.0.1:8889;charset=UTF8', 'root', 'root');
+
+        $sql="UPDATE `super_hero SET identite_secrete`=".$identite_secrete.", `id_super_pouvoir=`".$id_super_pouvoir." WHERE `nom`=".$nom.";";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+
+        return true;
+    }
+    catch (PDOException $e)
+    {
+        echo $e->getMessage();
+        return false;
+    }
+}
